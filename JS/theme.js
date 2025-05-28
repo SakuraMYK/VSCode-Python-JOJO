c0150e72c1664fea30770edfd235de6a858b3062
SakuraMYK <1003737847@qq.com>
2025-05-28 17:36:49 +0800

    feat(theme): 优化主题样式和代码高亮
    - 为私有变量、私有方法和魔术方法添加不同的高亮样式
    - 调整暗黑主题下的颜色设置，提高可读性
    - 优化函数和类定义的高亮样式

--- JS/theme.js
+++ JS/theme.js
@@ -15,30 +15,51 @@ async function applyTheme(themeName) {
 
 class PrivateHighlight {
   constructor() {
-    this.decorationType = vscode.window.createTextEditorDecorationType({
+    this.privateVarStyle = vscode.window.createTextEditorDecorationType({
       color: "#8EFA58",
       fontWeight: "bold",
       fontStyle: "italic",
     });
-    this.updateTimeout = null;
+    this.privateMethodStyle = vscode.window.createTextEditorDecorationType({
+      color: "#576EFF",
+      fontWeight: "bold",
+      fontStyle: "italic",
+    });
+    
+    this.magicMethodStyle = vscode.window.createTextEditorDecorationType({
+      color: "#67B8EA",
+      fontWeight: "bold",
+      fontStyle: "",
+    });
   }
 
-  update(editor) {
+  _setDecorations(editor, regex, style) {
     const text = editor.document.getText();
     const decorations = [];
-    const regex = /self\._\w+/g;
     let match;
 
     while ((match = regex.exec(text)) !== null) {
-      const startPos = editor.document.positionAt(match.index);
-      const endPos = editor.document.positionAt(match.index + match[0].length);
+      const start = match.index + match[1].length;
+      const end = start + match[2].length;
 
       decorations.push({
-        range: new vscode.Range(startPos, endPos),
+        range: new vscode.Range(
+          editor.document.positionAt(start),
+          editor.document.positionAt(end)
+        ),
       });
     }
+    editor.setDecorations(style, decorations);
+  }
 
-    editor.setDecorations(this.decorationType, decorations);
+  update(editor) {
+    const rePrivateVar = /(self\s*\.\s*)(_\w+)/g;
+    const rePrivateMethod = /(def\s+)(_(?!_)\w+)/g;
+    const reMagicMethod = /(def\s+)(__\w+__)/g;
+
+    this._setDecorations(editor, rePrivateVar, this.privateVarStyle);
+    this._setDecorations(editor, rePrivateMethod, this.privateMethodStyle);
+    this._setDecorations(editor, reMagicMethod, this.magicMethodStyle);
   }
 }
 
